b37edabe046e73bdba08272b427be6d4e2cbeb24
Josep Servat <jservatlorca@gmail.com>
2019-02-06 03:36:44 +0100

    First Dummy Test Passing

--- test/unit/store.test.js
+++ test/unit/store.test.js
@@ -1,6 +1,7 @@
 const store = require('../../lib/store');
 
 describe('store', () => {
+  console.log(store);
   test("Create a store for log created events", () => {
     store.createStore();
   });
