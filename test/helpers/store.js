4568390b5cf997f2c89f90e559d6eb66526b6f6b
Josep Servat <jservatlorca@gmail.com>
2019-02-06 03:10:43 +0100

    add a bit more structure before DI

---
+++ test/helpers/store.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+
+const checkTable = (table)  => {
+
+}
+
+module.exports = () => {
+  checkTable
+}
---
+++ test/unit/store.test.js
@@ -0,0 +1,7 @@
+const store = require('../../lib/store');
+
+describe('store', () => {
+  test("Create a store for log created events", () => {
+    store.createStore();
+  });
+})
